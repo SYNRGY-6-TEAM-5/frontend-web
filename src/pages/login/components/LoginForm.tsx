d8a4bf9e3a8686ae415102d721d6acd4f48a1f3b
Rutri <rutri.ayutya@gmail.com>
2024-01-05 09:50:28 +0700

    feat: menambahkan layout login

--- src/pages/login/components/LoginForm.tsx
+++ src/pages/login/components/LoginForm.tsx
@@ -2,85 +2,83 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { cn } from "@/lib/utils";
-import { Chrome, Facebook, Github, Loader } from "lucide-react";
+import { Loader } from "lucide-react";
 import { useState } from "react";
+import { Link } from "react-router-dom"
 
 interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 const LoginForm = ({ className, ...props }: UserAuthFormProps) => {
   const [isPending, setIsPending] = useState<boolean>();
 
+  const handleOnSubmit = async (e: React.SyntheticEvent) => {
+    setIsPending(true);
+		e.preventDefault();
+		const target = e.target as typeof e.target & {
+      email: { value: string };
+      password: { value: string };
+    };
+
+		const email = target.email.value;
+		const password = target.password.value;
+
+		console.log({email, password});
+    setIsPending(false);
+	};
+
   return (
     <div className={cn("grid gap-6", className)} {...props}>
       <form
-        onSubmit={() => {
-          setIsPending(true);
-        }}
+        onSubmit={handleOnSubmit}
       >
-        <div className="grid gap-2">
-          <div className="grid gap-1">
-            <Label className="sr-only" htmlFor="email">
-              Email
-            </Label>
-            <Input
-              id="email"
-              placeholder="Your email here..."
-              type="email"
-              autoCapitalize="none"
-              autoComplete="off"
-              autoCorrect="off"
-            />
+        <div className="grid gap-6">
+          <div className="grid gap-5">
+            <div className="grid gap-1.5">
+              <Label htmlFor="email">
+                Email / Phone Number
+              </Label>
+              <Input
+                id="email"
+                placeholder="Enter your email / phone number"
+                type="text"
+                autoCapitalize="none"
+                autoComplete="off"
+                autoCorrect="off"
+                className="border-0 border-b border-b-grey-500 rounded-none py-2.5 px-0 text-base"
+              />
+            </div>
+            <div className="grid gap-1">
+              <Label htmlFor="password">
+                Password
+              </Label>
+              <Input
+                id="password"
+                placeholder="Enter your password"
+                type="password"
+                autoCapitalize="none"
+                autoComplete="off"
+                autoCorrect="off"
+                className="border-0 border-b border-b-grey-500 rounded-none py-2.5 px-0 text-base"
+              />
+            </div>
           </div>
-          <div className="grid gap-1">
-            <Label className="sr-only" htmlFor="password">
-              Password
-            </Label>
-            <Input
-              id="password"
-              placeholder="Your password here..."
-              type="password"
-              autoCapitalize="none"
-              autoComplete="off"
-              autoCorrect="off"
-            />
-          </div>
-          <Button type="submit">
+          <Link to={'/forgot-password'} className="text-right text-gray-400 text-sm text-medium">
+            Forgot Password
+          </Link>
+          <Button type="submit" className="py-4 bg-primary-500 text-white rounded-xl">
             {isPending && <Loader className="mr-2 h-4 w-4 animate-spin" />}
-            Sign In with Email
+            Sign In 
           </Button>
         </div>
       </form>
-      <div className="relative">
-        <div className="relative flex justify-center text-xs uppercase">
-          <span className="bg-background text-muted-foreground px-2">
-            Or continue with
-          </span>
-        </div>
-      </div>
-      <div className="flex justify-between">
-        <Button variant="outline" type="button" disabled={isPending}>
+      <div className="grid">
+        <Button variant="outline" type="button" disabled={isPending} className="rounded-xl border-gray-200 gap-2">
           {isPending ? (
             <Loader className="mr-2 h-4 w-4 animate-spin" />
           ) : (
-            <Chrome className="mr-2 h-4 w-4" />
+            <img src="./src/assets/google.svg" />
           )}
-          Google
-        </Button>
-        <Button variant="outline" type="button" disabled={isPending}>
-          {isPending ? (
-            <Loader className="mr-2 h-4 w-4 animate-spin" />
-          ) : (
-            <Github className="mr-2 h-4 w-4" />
-          )}
-          Github
-        </Button>
-        <Button variant="outline" type="button" disabled={isPending}>
-          {isPending ? (
-            <Loader className="mr-2 h-4 w-4 animate-spin" />
-          ) : (
-            <Facebook className="mr-2 h-4 w-4" />
-          )}
-          Facebook
+          Sign in with Google
         </Button>
       </div>
     </div>
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,77 +1,48 @@
-import { cn } from "@/lib/utils";
 import { Link } from "react-router-dom";
 import LoginForm from "./components/LoginForm";
-import { buttonVariants } from "@/components/ui/button";
 
 const Login = () => {
   return (
     <>
-      <div className="container relative hidden h-[100vh] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
-        <Link
-          to="/examples/authentication"
-          className={cn(
-            buttonVariants({ variant: "ghost" }),
-            "absolute right-4 top-4 md:right-8 md:top-8",
-          )}
-        >
-          Login
-        </Link>
-        <div className="bg-muted relative hidden h-full flex-col p-10 text-white lg:flex dark:border-r">
-          <div className="absolute inset-0 bg-zinc-900" />
-          <div className="relative z-20 flex items-center text-lg font-medium">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="mr-2 h-6 w-6"
-            >
-              <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
-            </svg>
-            Acme Inc
-          </div>
-          <div className="relative z-20 mt-auto">
-            <blockquote className="space-y-2">
-              <p className="text-lg">
-                &ldquo;This library has saved me countless hours of work and
-                helped me deliver stunning designs to my clients faster than
-                ever before.&rdquo;
-              </p>
-              <footer className="text-sm">Sofia Davis</footer>
-            </blockquote>
-          </div>
+      <div className="container relative hidden md:h-[100vh] flex-col items-center justify-center lg:max-w-none lg:grid-cols-2 lg:p-0 min-[320px]:grid min-[320px]:py-5">
+        <div className="lg:bg-login-bg bg-cover relative hidden h-full flex-col p-10 text-white lg:flex dark:border-r">
         </div>
-        <div className="lg:p-8">
+        <div className="lg:p-8 space-y-24">
           <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
-            <div className="flex flex-col space-y-2 text-center">
-              <h1 className="text-2xl font-semibold tracking-tight">
-                Login to your account
+            <div className="flex flex-col space-y-3 text-center">
+              <div className="mx-auto mb-3">
+                <img src="./src/assets/Vector.svg"/>
+              </div>
+              <h1 className="text-3xl font-semibold tracking-tight">
+                Welcome back
               </h1>
-              <p className="text-muted-foreground text-sm">
-                Enter your email below to create your account
+              <p className="text-muted-foreground text-sm text-gray-400">
+                Welcome back! Please enter your details.
               </p>
             </div>
             <LoginForm />
             <p className="text-muted-foreground px-8 text-center text-sm">
-              By clicking continue, you agree to our{" "}
+              Don’t have an account?{" "}
               <Link
-                to="/terms"
-                className="hover:text-primary underline underline-offset-4"
+                to="/register"
+                className="hover:text-primary text-primary-500 font-semibold"
               >
-                Terms of Service
-              </Link>{" "}
-              and{" "}
-              <Link
-                to="/privacy"
-                className="hover:text-primary underline underline-offset-4"
-              >
-                Privacy Policy
+                Sign up
               </Link>
-              .
             </p>
+          </div>
+          <div className="flex sm:w-6/12 flex-col justify-center mx-auto text-center text-wrap min-[320px]:px-6 min-[320px]:pb-8">
+            <div className="relative z-20 mt-auto">
+              <blockquote className="space-y-2">
+                <p className="text-xs text-center">
+                  By continuing you agree to our
+                  <Link to="/term" className="text-primary-500 font-medium">
+                    T&Cs
+                  </Link>
+                  . We use your data to offer you a personalized experience
+                </p>
+              </blockquote>
+            </div>
           </div>
         </div>
       </div>
