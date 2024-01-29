ce0af3816c8a88bfb85852afd37c64197abc534c
Saifulloh Fadli <saifulloh.fadli@gmail.com>
2024-01-29 12:18:18 +0700

    feat: COntact Details Form

--- src/pages/booking/components/contact-detail/index.tsx
+++ src/pages/booking/components/contact-detail/index.tsx
@@ -1,18 +1,29 @@
+import { Text } from "@mantine/core";
+
 import {
   Card,
-  CardDescription,
-  CardHeader,
-  CardTitle,
+  CardContent,
+  CardFooter
 } from "@/components/ui/card";
+import ContactDetailsForm from "./components/ContactDetailsForm";
 
 const ContactDetail = () => {
   return (
+    <section className="flex flex-col gap-2">
+      <div className="px-2">
+        <Text className="pb-2 text-lg font-normal text-black">
+          Contact Details
+        </Text>
+      </div>
     <Card className="mb-8">
-      <CardHeader>
-        <CardTitle>Contact Detail</CardTitle>
-        <CardDescription>Card Description</CardDescription>
-      </CardHeader>
+      <CardContent className="px-6 py-8">
+        <ContactDetailsForm />
+      </CardContent>
+      <CardFooter className="text-sm font-normal text-slate-800">
+        Ticket information and confirmation will be sent to the contact details listed
+      </CardFooter>
     </Card>
+    </section>
   );
 };
 
