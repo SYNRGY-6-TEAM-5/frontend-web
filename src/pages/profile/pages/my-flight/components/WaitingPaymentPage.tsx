1c7b786546a9a28dc5a145ef9e8824a406b55e5d
Saifulloh Fadli <saifulloh.fadli@gmail.com>
2024-02-12 15:07:34 +0700

    feat: Fix Timer Hook, nyesuaiin karna habis diubah

--- src/pages/profile/pages/my-flight/components/WaitingPaymentPage.tsx
+++ src/pages/profile/pages/my-flight/components/WaitingPaymentPage.tsx
@@ -1,31 +1,65 @@
 import { ChevronLeft } from "lucide-react";
 import TimerMyFlight from "./ui/TimerMyFlight";
-import useTimer from "@/lib/hooks/useTimer";
+import useParseTime from "@/lib/hooks/useTimer";
 import DetailRuteOrder from "./containers/DetailRuteOrder";
 import PassangerDetails from "./containers/PassangerDetails";
 import { Button, Text } from "@mantine/core";
 import { useLocation, useNavigate } from "react-router-dom";
 import { data } from "@/components/particles/BookingData";
+import { useEffect, useState } from "react";
+import { differenceInSeconds, isFuture } from "date-fns";
+import useTimer from 'easytimer-react-hook';
+import useNav from "@/lib/hooks/useNav";
 
 const WaitingPayment = () => {
+  const [count_down, setCountDown] = useState<number>(0);
+  const [isRunOut, setIsRunOut] = useState<boolean>(false);
+  
+  const { userData } = useNav();
+
+  const [timer] = useTimer({ countdown: true });
 
   const location = useLocation();
   const orderId = location.state.orderId;
 
-  const dataBooking =data.filter(bookingUser => bookingUser.booking_id === orderId);
+  const dataBooking = data.filter(bookingUser => bookingUser.booking_id === orderId);
   const dataFiltered = dataBooking[0];
-  
-  const expiredSecond = new Date(dataFiltered.expired_time).getTime();
-  const date = new Date().getTime();
-  const countDownTime =expiredSecond - date;
-  const { seconds, minutes, hours} = useTimer({date, countDownTime});
+
+  const { seconds, minutes, hours } = useParseTime({ countDownTime: count_down });
 
   const navigate = useNavigate();
+
   const handleOnClick = () => {
     console.log("hello");
     navigate('/profile/');
   }
-  return(
+
+  const handlePay = () => {
+    if (userData) {
+      navigate(`/user/payment/${userData.id}/${dataBooking[0].booking_id}`);
+    } else {
+      console.log(userData)
+    }
+  };
+
+  useEffect(() => {
+    const expiryTime = data[0].expired_time;
+    const currentTime = new Date();
+
+    if (expiryTime && isFuture(new Date(expiryTime))) {
+      setCountDown(differenceInSeconds(new Date(expiryTime), currentTime));
+      setIsRunOut(false);
+    } else {
+      setIsRunOut(true);
+    }
+
+    timer.start({
+      countdown: true,
+      startValues: { hours, minutes, seconds },
+    });
+  }, [data, seconds, minutes, hours]);
+
+  return (
     <section id="waitingPayment">
       <div className="grid grid-cols-3 mb-10 items-center">
         <label
@@ -40,20 +74,23 @@ const WaitingPayment = () => {
           />
           <ChevronLeft size={20} />
         </label>
-        
         <div className="flex flex-col text-center sm:max-lg:col-span-2">
           <Text className="font-medium">Waiting For Payment</Text>
-          <Text className=" text-xs font-normal text-gray-400">Order ID:</Text>
+          <Text className=" text-xs font-normal text-gray-400">Order ID: {dataBooking[0].booking_id}</Text>
         </div>
       </div>
       <div className="flex flex-col gap-8">
-        <TimerMyFlight hours={hours} minutes={minutes} seconds={seconds} />
+        <TimerMyFlight timeValues={timer.getTimeValues()} />
         <DetailRuteOrder BookingUser={dataFiltered} />
         <PassangerDetails Passangers={dataFiltered.passengers} Tickets={dataFiltered.tickets} />
-        <Button
-          type="button"
-          className="w-full h-14 rounded-xl bg-primary-500 py-4 text-white font-medium text-sm"
-        >Complete the Payment in {hours}:{minutes}:{seconds}</Button>
+        {!isRunOut && (
+          <Button
+            onClick={handlePay}
+            type="button"
+            className="w-full h-14 rounded-xl bg-primary-500 py-4 text-white font-medium text-sm"
+          >
+            Complete the Payment in {hours}:{minutes}:{seconds}
+          </Button>)}
       </div>
     </section>
   )
--- src/pages/profile/pages/my-flight/components/ui/WaitingBtn.tsx
+++ src/pages/profile/pages/my-flight/components/ui/WaitingBtn.tsx
@@ -1,16 +1,23 @@
-import useTimer from "@/lib/hooks/useTimer";
+import useParseTime from "@/lib/hooks/useTimer";
 import { Button, Text } from "@mantine/core"
+import { differenceInSeconds, isFuture } from "date-fns";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import useTimer from 'easytimer-react-hook';
 
-const WaitingBtn = ({expiredTime, orderId, total}:{expiredTime:string, orderId:number, total:string}) => {
-  const expiredSecond = new Date(expiredTime).getTime();
-  const date = new Date().getTime();
-  const countDownTime = expiredSecond - date;
-  const { seconds, minutes, hours} = useTimer({date, countDownTime});
+const WaitingBtn = ({ expiredTime, orderId, total }: { expiredTime: string, orderId: number, total: string }) => {
+  const [count_down, setCountDown] = useState<number>(0);
+  const [isRunOut, setIsRunOut] = useState<boolean>(false);
+
+  const expiryTime = expiredTime;
+  const currentTime = new Date();
+
+  const [timer] = useTimer({ countdown: true });
+  const { seconds, minutes, hours } = useParseTime({ countDownTime: count_down });
 
   const navigate = useNavigate();
-  const handleWaiting = (orderId:number) => {
-    console.log({orderId});
+  const handleWaiting = (orderId: number) => {
+    console.log({ orderId });
     navigate('/profile/payment/', {
       state: {
         orderId: orderId,
@@ -18,8 +25,22 @@ const WaitingBtn = ({expiredTime, orderId, total}:{expiredTime:string, orderId:n
     });
   }
 
-  if(seconds === "00" && minutes === "00" && hours ==="00") {
-    return(
+  useEffect(() => {
+    if (expiryTime && isFuture(new Date(expiryTime))) {
+      setCountDown(differenceInSeconds(new Date(expiryTime), currentTime));
+      setIsRunOut(false);
+    } else {
+      setIsRunOut(true);
+    }
+
+    timer.start({
+      countdown: true,
+      startValues: { hours, minutes, seconds },
+    });
+  }, [seconds, minutes, hours]);
+
+  if (seconds === 0 && minutes === 0 && hours === 0) {
+    return (
       <>
         <div className="flex justify-between">
           <Text className="font-medium text-sm">Total</Text>
@@ -29,22 +50,26 @@ const WaitingBtn = ({expiredTime, orderId, total}:{expiredTime:string, orderId:n
           type="button"
           className="w-full h-14 rounded-xl bg-gray-300 py-4 text-white font-medium text-sm"
           disabled
-        >Complete the Payment in {hours}:{minutes}:{seconds}</Button>
+        >Complete the Payment in {timer.getTimeValues().toString(['hours', 'minutes', 'seconds'])}</Button>
       </>
     )
   } else {
 
-    return(
+    return (
       <>
         <div className="flex justify-between">
           <Text className="font-medium text-sm">Total</Text>
           <Text className="font-semibold text-primary-500">IDR {total}</Text>
         </div>
-        <Button
-          type="button"
-          onClick={() => handleWaiting(orderId)}
-          className="w-full h-14 rounded-xl bg-primary-500 py-4 text-white font-medium text-sm"
-        >Complete the Payment in {hours}:{minutes}:{seconds}</Button>
+        {!isRunOut && (
+          <Button
+            type="button"
+            onClick={() => handleWaiting(orderId)}
+            className="w-full h-14 rounded-xl bg-primary-500 py-4 text-white font-medium text-sm"
+          >
+            Complete the Payment in {timer.getTimeValues().toString(['hours', 'minutes', 'seconds'])}
+          </Button>
+        )}
       </>
     )
   }
